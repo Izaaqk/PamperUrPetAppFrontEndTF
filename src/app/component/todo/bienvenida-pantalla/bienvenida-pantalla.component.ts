e20166b5462f5b378b9343ce274e049a7b07ffa7
AndreaCZ16 <tokio.andrea.1612@gmail.com>
2023-11-12 18:21:49 -0500

    Update bienvenida-pantalla.component.ts

--- src/app/component/todo/bienvenida-pantalla/bienvenida-pantalla.component.ts
+++ src/app/component/todo/bienvenida-pantalla/bienvenida-pantalla.component.ts
@@ -25,5 +25,24 @@ export class BienvenidaPantallaComponent implements OnInit{
   });
     
   }
+  onImageClick(event: any) {
+    // Aquí puedes implementar la lógica para seleccionar una imagen y guardarla
+    // Puedes abrir un cuadro de diálogo para que el usuario seleccione una imagen o implementar tu lógica
+    // En este ejemplo, asumiré que el usuario selecciona un archivo de entrada de tipo 'file'
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/*';
+  
+    input.onchange = (e: any) => {
+      const file = e.target.files[0];
+      if (file) {
+        const correoPropietario = 'correo@example.com'; // Reemplaza esto con el verdadero correo del propietario
+        // Establece la imagen seleccionada usando el servicio compartido
+        this.sharedDataService.setImagenSeleccionada(correoPropietario, file);
+      }
+    };
+  
+    input.click();
+  }
 
 }
